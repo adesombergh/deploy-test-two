894131941ce168f0b84186d7eecad69c2b9b78cc
adesombergh <adesombergh@MBPdeadsombergh.lan>
2018-04-09 16:27:27 +0200

    GO

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('Hello World!!!!');
 });
 
-var server = app.listen(3001, function(){
-  console.log('Magic is happening on port 3001')
+var server = app.listen(3002, function(){
+  console.log('Magic is happening on port 3002')
 });
